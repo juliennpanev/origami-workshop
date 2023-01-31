817ece2b47adc6cb05707dda52c7d51246fb4a76
juliennpanev <83950947+juliennpanev@users.noreply.github.com>
2023-01-31 13:54:22 +0200

    commited from windows

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Let's begin</h1>
+		</div>
+	);
 }
 
 export default App;
